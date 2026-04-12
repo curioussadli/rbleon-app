f260881323b127cdbcc980228149bf1794edb5c0
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2026-04-12 10:10:33 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 // =============================
-// CONFIG FIREBASE
+// CONFIG
 // =============================
 const firebaseConfig = {
   apiKey: "AIzaSyCE7nPHUvlYTlbX654Iq7pA62eNQeDmKxY",
@@ -20,76 +16,11 @@ const firebaseConfig = {
 };
 
 // =============================
-// INIT FIREBASE
+// INIT
 // =============================
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 console.log("🔥 Firebase connect OK");
-
-// =============================
-// EXPORT YANG DIPAKAI APP
-// =============================
-export { db, collection, onSnapshot, addDoc };
-
-
-import { getAuth, GoogleAuthProvider, signInWithPopup } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-export function loginWithGoogle() {
-  return signInWithPopup(auth, provider);
-}
-
-
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  projectId: "XXX",
-  storageBucket: "XXX",
-  messagingSenderId: "XXX",
-  appId: "XXX"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-
-import {
-  GoogleAuthProvider,
-  signInWithPopup
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import {
-  doc,
-  setDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-import { auth, db } from "./firebase.js";
-
-const provider = new GoogleAuthProvider();
-
-export async function loginWithGoogle() {
-  const result = await signInWithPopup(auth, provider);
-
-  const user = result.user;
-
-  // 🔥 SIMPAN KE FIRESTORE
-  await setDoc(doc(db, "users", user.uid), {
-    name: user.displayName,
-    email: user.email,
-    photo: user.photoURL,
-    lastLogin: new Date()
-  }, { merge: true });
-
-  return user;
-}
