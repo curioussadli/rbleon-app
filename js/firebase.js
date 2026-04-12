d8f31b725204963b2d8c0334bcef13e3646e5644
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2026-04-12 13:14:35 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,13 +1,33 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const app = initializeApp({
+// =============================
+// CONFIG FIREBASE
+// =============================
+const firebaseConfig = {
   apiKey: "AIzaSyCE7nPHUvlYTlbX654Iq7pA62eNQeDmKxY",
   authDomain: "rblite-app.firebaseapp.com",
   projectId: "rblite-app",
+  storageBucket: "rblite-app.firebasestorage.app",
+  messagingSenderId: "373754561853",
   appId: "1:373754561853:web:56bdc30bdfe3ccec30b154",
-});
+  measurementId: "G-WQ950MP9E9"
+};
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// =============================
+// INIT FIREBASE
+// =============================
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+console.log("🔥 Firebase connect OK");
+
+// =============================
+// EXPORT YANG DIPAKAI APP
+// =============================
+export { db, collection, onSnapshot, addDoc };
