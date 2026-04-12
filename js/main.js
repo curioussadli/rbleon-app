066a92212d6606cce5d7d9f218bd7021dd7610cc
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2026-04-12 12:13:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,32 +15,7 @@ window.addEventListener("beforeinstallprompt", (e) => {
 // =============================
 import { loginWithGoogle } from "./auth.js";
 
-import { auth } from "./firebase.js";
-import { onAuthStateChanged } 
-from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-const googleBtn = document.getElementById("googleLogin");
-
-// 🔥 GLOBAL LISTENER (JALAN SEKALI SAJA)
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("Already logged in:", user.email);
-    window.location.href = "dashboard.html";
-  }
+document.getElementById("googleLogin").addEventListener("click", async () => {
+  await loginWithGoogle();
+  window.location.href = "dashboard.html";
 });
-
-if (googleBtn) {
-  googleBtn.addEventListener("click", async () => {
-    try {
-      await loginWithGoogle();
-
-      // 🔥 kasih waktu Firebase sync session
-      setTimeout(() => {
-        window.location.href = "dashboard.html";
-      }, 300);
-
-    } catch (err) {
-      console.error(err);
-    }
-  });
-}
