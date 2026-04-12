94368de0d0299a303f0258ecd1044bab0278338a
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2026-04-12 10:05:26 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -43,3 +43,53 @@ export function loginWithGoogle() {
   return signInWithPopup(auth, provider);
 }
 
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+
+import {
+  GoogleAuthProvider,
+  signInWithPopup
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+import { auth, db } from "./firebase.js";
+
+const provider = new GoogleAuthProvider();
+
+export async function loginWithGoogle() {
+  const result = await signInWithPopup(auth, provider);
+
+  const user = result.user;
+
+  // 🔥 SIMPAN KE FIRESTORE
+  await setDoc(doc(db, "users", user.uid), {
+    name: user.displayName,
+    email: user.email,
+    photo: user.photoURL,
+    lastLogin: new Date()
+  }, { merge: true });
+
+  return user;
+}
