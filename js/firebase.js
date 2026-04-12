105da252f18e1154fba6cc782f757b408951bad6
Sadli | Learn Data Science & AI/ML <176941072+curioussadli@users.noreply.github.com>
2026-04-12 12:22:29 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,16 +2,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-const firebaseConfig = {
+const app = initializeApp({
   apiKey: "AIzaSyCE7nPHUvlYTlbX654Iq7pA62eNQeDmKxY",
   authDomain: "rblite-app.firebaseapp.com",
   projectId: "rblite-app",
   appId: "1:373754561853:web:56bdc30bdfe3ccec30b154",
-};
+});
 
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-console.log("Firebase OK");
+export const db = getFirestore(app);
